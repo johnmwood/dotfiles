deeb0ad6cb42e6e9d4335609bcff0eb02448c1fe
John Wood <jwood@spotify.com>
2022-07-19 09:11:27 -0500

    edit settings with et=false

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,24 +1,23 @@
 local o = vim.o
 local opt = vim.opt
 
-o.relativenumber = true
-o.number = true
-o.hlsearch = false
-o.incsearch = true
-o.smartcase = true
-o.ignorecase = true
-o.tabstop = 4
-o.softtabstop = 4
-o.expandtab = true
-o.shiftwidth = 4
 o.autoindent = 4
-opt.scrolloff = 24
+o.et = false
+o.expandtab = true
+o.guitablabel = '%t'
+o.ignorecase = true
+o.incsearch = true
+o.number = true
+o.relativenumber = true
+o.shiftwidth = 4
+o.smartcase = true
+o.softtabstop = 4
+-- o.swapfile = true
+o.tabstop = 4
 o.ttyfast = true
 o.wrap = false
+
+opt.scrolloff = 24
 opt.mouse = 'a'
+
 vim.cmd 'set guicursor=i:ver25,i:blinkon1'
-o.guitablabel = '%t'
--- o.swapfile = true
--- o.nobackup = true
--- ctrl + w + o == close all windows except one 
--- ctrl + w + H == verticle to horizontal split 
