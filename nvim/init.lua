70134f28b3efb5eac1633ba350e5ab055f27735d
John Wood <johnmorgan@cloudflare.com>
2024-04-18 14:30:29 -0500

    highlight on yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -350,6 +350,14 @@ function! GetSourceLink() range
 endfunction
 ]]
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
 vim.keymap.set('', '<leader>bb', ':call GetSourceLink()<CR>')
 
 -- trouble
