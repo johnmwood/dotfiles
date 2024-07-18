bea027f03048a4809838ecdf4823258d71fbdc16
John Wood <johnmorgan@cloudflare.com>
2024-07-18 09:37:04 -0500

    update zed config

--- zed/copy-settings.sh
+++ zed/copy-settings.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 cp ~/.config/zed/settings.json .
+
+git diff
+while true; do
+    read -r -p "Do you want to commit this change? " yn
+    case $yn in
+        [Yy]* ) git add . && git commit -m "update zed config"; break;;
+        [Nn]* ) echo "no"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
